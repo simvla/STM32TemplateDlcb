8713c7a4c2d76463ce564dcc1ae5bb90ddd6d62f
Vladimir <50554409+simvla@users.noreply.github.com>
2019-05-17 21:48:23 +0200

    Update main.c

--- main.c
+++ main.c
@@ -17,18 +17,18 @@
 /* USER Program control flow---------------------------------------*/
 int main(void)
 {
-  /* StartupSetup-------------------------------------------------*/
-  //System 
-  HAL_Init(); //"hardwareAbstractionLayerLib initialize"-call for each new proj
-  SystemClock_Config(); //set sysClock(72MHz)-call for each new proj
-  //User 
+  /* ProjConfiguration-------------------------------------------------------------*/ 
+  //System config(important calls for each project)
+  HAL_Init(); //"hardwareAbstractionLayerLib" initialize
+  SystemClock_Config(); //set sysClock(72MHz)
+  //User config(project specific)
   OnBoardLED_Init();//set Led port(PC13)
   TFT_LCD_Init();//set tftLcd gpio and init sequence 
 
-  //Do on startup
+  //Startup code
   TFT_LCD_DisplayStringAtLine(1,(uint8_t*)"WELCOME!"); //print message on tftLcd
 
-  /* Main Loop CyclicExecution-------------------------------------*/
+  /* Main Loop CyclicExecution-------------------------------------------------*/
   while(1){
    //Led blink 
    HAL_GPIO_TogglePin(LED_PORT, LED_PIN);
